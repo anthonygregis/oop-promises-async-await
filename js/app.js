223917971b1aaac19632d4bdf21adb0b451b178b
Anthony Gregis <anthony@thedevtron.com>
2020-07-23 16:45:48 -0500

    [JS] Add constructor functions and examples

--- js/app.js
+++ js/app.js
@@ -39,3 +39,41 @@ console.log(playerStarterStats)
 console.log(player1)
 
 // Constructor functions
+
+function User(name) {
+    this.name = name
+
+    // return this
+}
+
+let adam = new User('Adam')
+let pete = new User('Pete')
+
+console.log(adam)
+console.log(pete)
+
+function NBAPlayer(name, team, threePointShooter) {
+    this.name = name
+    this.team = team
+    this.threePointShooter = threePointShooter
+}
+
+let steph = new NBAPlayer("Steph Curry", "Warriors", true)
+
+console.log(steph)
+
+function PlayerCharacter(name, strength, levelUnlock, weapon, health) {
+    this.name = name
+    this.strength = strength
+    this.levelUnlock = levelUnlock
+    this.weapon = weapon
+    this.health = health
+}
+
+let masterChief = new PlayerCharacter("Master Chief", "1000", "5", "Battle Rifle", "1000")
+let link = new PlayerCharacter("Link", "100", "1", "Sword", "100")
+let mario = new PlayerCharacter("Mario", "100", "1", "Hat", "100")
+
+console.log(masterChief)
+console.log(link)
+console.log(mario)
